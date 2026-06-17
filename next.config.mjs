222340deaaa2bcb2ed5dbe49c4b74e66dbf0d940
Ron Bachar <ron.bachar@outlook.com>
2026-06-17 15:48:36 +0300

    indexing fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/shop/p/herbal-boost-balance",
+        destination: "/products/balance",
+        permanent: true,
+      },
+      {
+        source: "/shop/p/herbal-boost-vitality",
+        destination: "/products/vitality",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
